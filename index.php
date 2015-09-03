2e4bcb3ecf16225a0472e842dfd75d5a921d4739
nickgs <nickgs1@gmail.com>
2015-09-03 11:37:33 -0400

    Added auto-install logic to index.php

--- index.php
+++ index.php
@@ -17,6 +17,15 @@ $kernel = new DrupalKernel('prod', $autoloader);
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+
+//make sure we have the database functions we need.
+include_once __DIR__ . '/core/includes/database.inc';
+//redirect to installer if we aren't yet installed and we are on stackstarter
+if (isset($_SERVER['STACKSTARTER_ENV']) && function_exists('db_table_exists') && !db_table_exists('sessions') && $_SERVER['SCRIPT_NAME'] != '/core/install.php') {
+  include_once __DIR__ . '/core/includes/install.inc';
+  install_goto('core/install.php');
+}
+
 $response->send();
 
 $kernel->terminate($request, $response);
